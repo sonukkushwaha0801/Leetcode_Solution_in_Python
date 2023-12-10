d3d76ad5901dc677a81f213e3846e20d9e66ec72
Sonu Kumar Kushwaha <144480219+sonukkushwaha0801@users.noreply.github.com>
2023-12-10 21:36:42 +0530

    Update 867. Transpose Matrix.py

--- 867. Transpose Matrix.py
+++ 867. Transpose Matrix.py
@@ -1,3 +1,20 @@
 # Visit the profile for more solutions with minimum complexity
 #https://leetcode.com/sonukkushwaha0801/
 #type first:
+class Solution:
+    def transpose(self, matrix: list[list[int]]) -> list[list[int]]:
+        return zip(*matrix)
+
+# Another way:
+class Solution(object):
+    def transpose(self, matrix):
+        row = len(matrix)
+        col = len(matrix[0])
+        result = [[0] * row for _ in range(col)]
+        
+        for i in range(col):
+            for j in range(row):
+                result[i][j] = matrix[j][i]
+                
+        return result
+        
