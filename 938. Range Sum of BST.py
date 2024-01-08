9c1b72789283b82ad1c8a9f73d902df8e7aa516e
Sonu Kumar Kushwaha <144480219+sonukkushwaha0801@users.noreply.github.com>
2024-01-08 18:58:10 +0530

    Add files via upload

---
+++ 938. Range Sum of BST.py
@@ -0,0 +1,32 @@
+# Visit the profile for more solutions with minimum complexity
+# https://leetcode.com/sonukkushwaha0801/
+# One way:
+class Solution:
+    def rangeSumBST(self, root, L, R):
+        if not root:
+            return 0
+        
+        if L <= root.val <= R:
+            return root.val + self.rangeSumBST(root.left, L, R) + self.rangeSumBST(root.right, L, R)
+        elif root.val < L:
+            return self.rangeSumBST(root.right, L, R)
+        else:
+            return self.rangeSumBST(root.left, L, R)
+
+# Another way:
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        def dfs(node):
+            if not node:
+                return 0
+            
+            current_val = 0
+            if low <= node.val <= high:
+                current_val = node.val
+            
+            left_sum = dfs(node.left)
+            right_sum = dfs(node.right)
+            
+            return current_val + left_sum + right_sum
+        
+        return dfs(root)
