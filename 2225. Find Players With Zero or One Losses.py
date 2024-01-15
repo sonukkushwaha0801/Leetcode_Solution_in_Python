39b41e938a7b478da12aa009e3893e35e6be6116
Sonu Kumar Kushwaha <144480219+sonukkushwaha0801@users.noreply.github.com>
2024-01-15 19:10:57 +0530

    Add files via upload

---
+++ 2225. Find Players With Zero or One Losses.py
@@ -0,0 +1,49 @@
+# Visit the profile for more solutions with minimum complexity
+# https://leetcode.com/sonukkushwaha0801/
+# One way:
+from typing import List
+
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        zeroLoss, oneLoss, moreLoss = set(), set(), set()
+
+        for match in matches:
+            winner, loser = match[0], match[1]
+
+            # Add winner.
+            if winner not in oneLoss and winner not in moreLoss:
+                zeroLoss.add(winner)
+
+            # Add or move loser.
+            if loser in zeroLoss:
+                zeroLoss.remove(loser)
+                oneLoss.add(loser)
+            elif loser in oneLoss:
+                oneLoss.remove(loser)
+                moreLoss.add(loser)
+            elif loser in moreLoss:
+                continue
+            else:
+                oneLoss.add(loser)
+
+        answer = [sorted(list(zeroLoss)), sorted(list(oneLoss))]
+        return answer
+
+#Another way:
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        loss={}
+        for p in matches:
+            x, y=p
+            if x not in loss: loss[x]=0
+            if y in loss: loss[y]+=1
+            else: loss[y]=1
+        ans=[[], []]
+        for i, f in loss.items():
+            if f==0:
+                ans[0].append(i)
+            elif f==1:
+                ans[1].append(i)
+        
+        return [sorted(ans[0]), sorted(ans[1])]
+        
