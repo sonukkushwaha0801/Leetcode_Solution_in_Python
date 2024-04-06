74d16d2bdc92716ac49c06dc9c48e8f22f1d1318
Sonu Kumar Kushwaha <144480219+sonukkushwaha0801@users.noreply.github.com>
2024-04-06 19:59:45 +0530

    Add files via upload

---
+++ 1249. Minimum Remove to Make Valid Parentheses.py
@@ -0,0 +1,40 @@
+# Visit the profile for more solutions with minimum complexity
+# https://leetcode.com/sonukkushwaha0801/
+# type first:
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        openParenthesesCount = 0
+        arr = list(s)
+        for i in range(len(arr)):
+            if arr[i] == '(':
+                openParenthesesCount += 1
+            elif arr[i] == ')':
+                if openParenthesesCount == 0:
+                    arr[i] = '*'
+                else:
+                    openParenthesesCount -= 1
+        for i in range(len(arr) - 1, -1, -1):
+            if openParenthesesCount > 0 and arr[i] == '(':
+                arr[i] = '*'
+                openParenthesesCount -= 1
+        result = ''.join(c for c in arr if c != '*')
+
+        return result
+    
+# Another way:
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack = []
+        s = list(s)
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            elif char == ')':
+                if stack:
+                    stack.pop()
+                else:
+                    s[i] = ''
+        while stack:
+            s[stack.pop()] = ''
+        
+        return ''.join(s)
