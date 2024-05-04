1bf932fe664c09a01632d6bbe99f64a60161842c
Sonu Kumar Kushwaha <144480219+sonukkushwaha0801@users.noreply.github.com>
2024-05-04 21:36:14 +0530

    Add files via upload

---
+++ 881. Boats to Save People.py
@@ -0,0 +1,18 @@
+# Visit the profile for more solutions with minimum complexity
+#https://leetcode.com/sonukkushwaha0801/
+# Type one:
+class Solution:
+    def numRescueBoats(self, people: list[int], limit: int) -> int:
+        people.sort() 
+        left, right = 0, len(people) - 1
+        
+        boats = 0
+        
+        while left <= right:
+            if people[left] + people[right] <= limit:
+                left += 1
+                right -= 1 
+            else:
+                right -= 1
+            boats += 1
+        return boats
