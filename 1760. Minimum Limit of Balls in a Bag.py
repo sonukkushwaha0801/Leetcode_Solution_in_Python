574d5d289ff6ff70b065b80b0b6b90393645c28b
Sonu Kumar Kushwaha <144480219+sonukkushwaha0801@users.noreply.github.com>
2024-12-07 22:32:43 +0530

    Add files via upload

---
+++ 1760. Minimum Limit of Balls in a Bag.py
@@ -0,0 +1,16 @@
+# Visit the profile for more solutions with minimum complexity
+#https://leetcode.com/sonukkushwaha0801/
+# Type one:
+
+from typing import List
+
+
+class Solution:
+    def minimumSize(self, nums: List[int], maxOps: int) -> int:
+        low, high = 1, max(nums) 
+        while low < high:
+            mid = (low + high) // 2
+            if sum((n - 1) // mid for n in nums) <= maxOps: high = mid
+            else: low = mid + 1
+        return high
+    
